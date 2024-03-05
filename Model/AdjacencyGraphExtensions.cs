064fb48ece0ffc7c0ff972a22ec78f0124d8ef08
aeiouxx <st55776@upce.cz>
2024-03-05 01:18:44 +0100

    killme

--- Model/AdjacencyGraphExtensions.cs
+++ Model/AdjacencyGraphExtensions.cs
@@ -2,15 +2,112 @@
 {
     public static class AdjacencyGraphExtensions
     {
-        //public static IEnumerable<List<TVertex>> FindAllPathsIterative<TVertex, TEdge>(this AdjacencyGraph<TVertex, TEdge> graph,
-        //    IEnumerable<TVertex> startingVertices,
-        //    IEnumerable<TVertex> endVertices)
-        //    where TVertex : notnull
-        //    where TEdge : IEdge<TVertex>
-        //{
-        //    var allPaths = new List<List<TVertex>>();
-        //    var stack = new Stack<(TVertex currentVertex, List<TVertex> Path, int? precursorIndex)>();
-        //}
+        public static List<List<List<TVertex>>> FindAllDisjointTuples<TVertex>(this AdjacencyGraph<TVertex, IEdge<TVertex>> graph,
+            IEnumerable<TVertex> startingVertices,
+            IEnumerable<TVertex> endVertices)
+            where TVertex : notnull
+        {
+            var allPaths = FindAllPathsIterative(graph, startingVertices, endVertices);
+            var maxTupleSize = Math.Min(startingVertices.Count(), endVertices.Count());
+            var validTuples = new List<List<List<TVertex>>>();
+            for (int n = 2; n <= maxTupleSize; n++)
+            {
+                var combinations = GetCombinations(allPaths.ToList(), n, 0);
+
+                foreach (var combo in combinations)
+                {
+                    if (ArePathsDisjoint(combo))
+                    {
+                        validTuples.Add(combo);
+                    }
+                }
+            }
+            return validTuples;
+        }
+
+        private static bool ArePathsDisjoint<TVertex>(List<List<TVertex>> combo) where TVertex : notnull
+        {
+            HashSet<TVertex> vertices = new();
+
+            foreach (var path in combo)
+            {
+                foreach (var vertex in path)
+                {
+                    if (vertices.Contains(vertex))
+                    {
+                        return false;
+                    }
+                    vertices.Add(vertex);
+                }
+            }
+
+            return true;
+        }
+
+        private static IEnumerable<List<List<TVertex>>> GetCombinations<TVertex>(List<List<TVertex>> paths, int length, int startIndex)
+            where TVertex : notnull
+        {
+            if (length == 1)
+            {
+                for (int i = startIndex; i < paths.Count; i++)
+                {
+                    yield return new List<List<TVertex>> { paths[i] };
+                }
+            }
+
+            else
+            {
+                for (int i = startIndex; i <= paths.Count - length; i++)
+                {
+                    foreach (var combo in GetCombinations(paths, length - 1, i + 1))
+                    {
+                        yield return new List<List<TVertex>> { paths[i] }.Concat(combo).ToList();
+                    }
+                }
+            }
+        }
+        public static IEnumerable<List<TVertex>> FindAllPathsIterative<TVertex, TEdge>(this AdjacencyGraph<TVertex, TEdge> graph,
+            IEnumerable<TVertex> startingVertices,
+            IEnumerable<TVertex> endVertices)
+            where TVertex : notnull
+            where TEdge : IEdge<TVertex>
+        {
+            List<List<TVertex>> allPaths = new();
+            HashSet<TVertex> goalSet = new(endVertices);
+            Stack<(List<TVertex> Path, Option<TVertex> precursor)> pathStack = new();
+            foreach (var startingVertex in startingVertices)
+            {
+                pathStack.Push((new List<TVertex> { startingVertex }, Option<TVertex>.None));
+            }
+            while (pathStack.Count > 0)
+            {
+                var (currentPath, lastVertex) = pathStack.Pop();
+                TVertex current = currentPath[^1];
+                // directed acyclic graph so the only way to have a "cycle" is a path of 1 vertex (start = goal)
+                if (goalSet.Contains(current)
+                    && currentPath.Count > 1)
+                {
+                    allPaths.Add(new List<TVertex>(currentPath));
+                }
+
+                foreach (var edge in graph.GetOutgoingEdges(current))
+                {
+                    if (edge is GauntletEdge<TVertex> gauntlet)
+                    {
+                        if (gauntlet.TraversibleFor.HasValue
+                               && gauntlet.TraversibleFor.Value.Equals(lastVertex.Value))
+                        {
+                            pathStack.Push((new List<TVertex>(currentPath) { edge.To }, current));
+                        }
+                    }
+                    else
+                    {
+                        pathStack.Push((new List<TVertex>(currentPath) { edge.To }, current));
+                    }
+                }
+            }
+            return allPaths;
+        }
         public static IEnumerable<List<TVertex>> FindAllPathsRecursive<TVertex, TEdge>(this AdjacencyGraph<TVertex, TEdge> graph,
             IEnumerable<TVertex> startingVertices,
             IEnumerable<TVertex> endVertices)
--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
 ﻿using RailSim.Model;
-using RailSim.Model.Persistence;
-using System.Text.Json;
 
 namespace RailSim
 {
@@ -10,26 +8,43 @@ namespace RailSim
         static void Main(string[] args)
         {
             var graph = GraphFactory.Create<string>();
+            graph.AddVertex("V23");
+            graph.AddVertex("V21");
+            graph.AddVertex("V22");
+            graph.AddVertex("V24");
+            graph.AddVertex("V12");
+            graph.AddVertex("V14");
+            graph.AddVertex("V30");
+            graph.AddVertex("V17");
+            graph.AddVertex("V29");
+            graph.AddVertex("V18");
+            graph.AddVertex("V27");
+            graph.AddVertex("V13");
+            graph.AddVertex("V15");
+            graph.AddVertex("V16");
+            graph.AddVertex("V19");
+            graph.AddVertex("V28");
 
-            graph.AddVertex("A");
-            graph.AddVertex("B");
-            graph.AddVertex("C");
-            graph.AddVertex("D");
-            graph.AddVertex("E");
+            graph.AddEdge(new Edge<string>("V23", "V12"));
+            graph.AddEdge(new Edge<string>("V21", "V14"));
+            graph.AddEdge(new Edge<string>("V22", "V15"));
+            graph.AddEdge(new Edge<string>("V24", "V13"));
+            graph.AddEdge(new Edge<string>("V13", "V15"));
+            graph.AddEdge(new Edge<string>("V15", "V16"));
+            graph.AddEdge(new Edge<string>("V12", "V14"));
+            graph.AddEdge(new Edge<string>("V14", "V30"));
+            graph.AddEdge(new Edge<string>("V30", "V17"));
+            graph.AddEdge(new Edge<string>("V17", "V29"));
+            graph.AddEdge(new Edge<string>("V29", "V18"));
+            graph.AddEdge(new Edge<string>("V18", "V27"));
+            graph.AddEdge(new Edge<string>("V18", "V19"));
+            graph.AddEdge(new Edge<string>("V19", "V28"));
+            graph.AddEdge(new Edge<string>("V16", "V19"));
+            graph.AddEdge(new Edge<string>("V16", "V17"));
 
-            graph.AddEdge(new Edge<string>("A", "C"));
-            graph.AddEdge(new Edge<string>("A", "D"));
-            graph.AddEdge(new Edge<string>("B", "C"));
-            graph.AddEdge(new Edge<string>("B", "E"));
 
-            graph.AddEdge(new GauntletEdge<string>("C", "D", "B"));
-            graph.AddEdge(new GauntletEdge<string>("C", "E", "A"));
-
-            graph.Print();
-            Console.ReadKey(intercept: true);
-
-            var startingVertices = new[] { "A", "B" };
-            var endVertices = new[] { "D", "E" };
+            var startingVertices = new[] { "V23", "V21", "V22", "V24", "V30", "V29" };
+            var endVertices = new[] { "V30", "V29", "V27", "V28" };
             Console.WriteLine($"Searching paths from: {string.Join(" ", startingVertices)}");
             Console.WriteLine($"To: {string.Join(" ", endVertices)}");
             Console.WriteLine($"Found: ");
@@ -38,24 +53,27 @@ namespace RailSim
             {
                 Console.WriteLine(string.Join(" -> ", path));
             }
-
-
-            var options = new JsonSerializerOptions
+            Console.WriteLine("---ITERATIVE FIND---");
+            var paths2 = graph.FindAllPathsIterative(startingVertices, endVertices);
+            foreach (var path in paths2)
             {
-                WriteIndented = true,
-                Converters =
-                {
-                    new EdgeConverter<string>(),
-                    new AdjacencyGraphConverter<string, IEdge<string>>()
-                }
-            };
-
-            string jsonText = JsonSerializer.Serialize(graph, options);
-            var filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "graph.json");
-            File.WriteAllText(filePath, jsonText);
-
-
-            var parsedGraph = JsonSerializer.Deserialize<AdjacencyGraph<string, IEdge<string>>>(jsonText, options);
+                Console.WriteLine(string.Join(" -> ", path));
+            }
+            Console.WriteLine("---DISJOINT TUPLES---");
+            var result = graph.FindAllDisjointTuples(startingVertices, endVertices);
+            //var options = new JsonSerializerOptions
+            //{
+            //    WriteIndented = true,
+            //    Converters =
+            //    {
+            //        new EdgeConverter<string>(),
+            //        new AdjacencyGraphConverter<string, IEdge<string>>()
+            //    }
+            //};
+            //string jsonText = JsonSerializer.Serialize(graph, options);
+            //var filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "graph.json");
+            //File.WriteAllText(filePath, jsonText);
+            //var parsedGraph = JsonSerializer.Deserialize<AdjacencyGraph<string, IEdge<string>>>(jsonText, options);
         }
     }
 }
